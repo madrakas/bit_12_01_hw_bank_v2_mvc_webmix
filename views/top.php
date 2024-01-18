aaf4bc314e8c684e67d284a62ec07dcb926acf3a
Arvydas <arvydas.simbelis@gmail.com>
2024-01-18 10:50:25 +0200

    Laravel mix

--- views/top.php
+++ views/top.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= URL ?>/main.css?v=<?= time() ?>">
     <title><?= $title ?? 'Untitled' ?></title>
 </head>
 <body>
---
+++ webpack.mix.js
@@ -0,0 +1,4 @@
+let mix = require('laravel-mix');
+
+mix
+.sass('resources/css/main.scss', 'public');
