fc4423365cba691c7eb22e32ed7122add25ad5dd
Arvydas <arvydas.simbelis@gmail.com>
2024-01-22 11:40:54 +0200

    User add account

---
+++ views/user/createaccount.php
@@ -0,0 +1,8 @@
+<h1>Create new money account</h1>
+
+
+
+<form action="<?= URL ?>/user/storeaccount" method='post'>
+<p>Create new money account?</p>
+    <button type="submit">Create account</button>
+</form> 
