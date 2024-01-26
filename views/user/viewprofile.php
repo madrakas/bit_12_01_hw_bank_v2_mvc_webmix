7b864b4595f7ae489b2bb004bc6dd75ce2b62b6e
Arvydas <arvydas.simbelis@gmail.com>
2024-01-26 23:26:07 +0200

    User view profile

---
+++ views/user/viewprofile.php
@@ -0,0 +1,11 @@
+<h1>User profile</h1>
+
+<p><strong>First name: </strong><?= $user->firstname ?>.</p>
+<p><strong>Last name: </strong><?= $user->lastname ?>.</p>
+<p><strong>Personal identification number: </strong> <?= $user->ak ?>.</p>
+<p><strong>Email address: </strong> <?= $user->email ?>.</p>
+
+<h2>Change your data</h2>
+    <p><a href="<?= URL ?>/user/editprofile">Edit personal information</a></p>
+    <p><a href="<?= URL ?>/userchangepw">Change password</a></p>
+    <p><a href="<?= URL ?>/user/delprofile">Close acount</a></p>
